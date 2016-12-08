10704546ef9d04bec8b329a503dca06797c05b1f
Kara Bernert <krbernert@gmail.com>
2016-12-08 13:10:20 -0500

    a whole bunch more stuff that should rly be ignored

--- src/spinner.elm
+++ src/spinner.elm
@@ -2,48 +2,74 @@ module Spinner exposing (..)
 
 import Html exposing (..)
 import Html.Events exposing (..)
--- import Html.Attributes exposing (..)
 import Svg exposing (..)
 import Svg.Attributes exposing (..)
+import Time exposing (Time)
+import Random
 
-type alias Model = { value : Int }
-type Msg
-    = RequestSpinMagnitude
-    | ReceiveSpinMagnitude Int
-    | Spin Int
-    | SpinComplete
 
-view : Model -> Html Msg
-view model = 
-  Svg.svg [viewBox "-200 -200 400 400", width "400px", height "400px"] 
-    [ Svg.circle [ fill "blue", cx "0", cy "0", r "200" ] [] 
-    , Svg.polygon [ points "10,20 -10,20 0,-20 10,20", stroke "orange", strokeWidth "3", fill "none", strokeLinejoin "round"] []
-    ]
-
-update : Msg -> Model -> (Model, Cmd Msg)
-update msg model = 
-  case msg of
-    RequestSpinMagnitude -> 
-      ( model, Cmd.none )
-    ReceiveSpinMagnitude mag ->
-      ( model, Cmd.none )
-    Spin ->
-      ( model, Cmd.none )
-    SpinComplete ->
-      ( model, Cmd.none )
-
-initialModel : Model
-initialModel = { value = 1 }
-
-main =
-  Html.program
-    { init = ( initialModel, Cmd.none )
-    , view = view
-    , update = update
-    , subscriptions = \_ -> Sub.none
+type alias Model =
+    { theta : Int
+    , oomph : Int
     }
 
 
+type Msg
+    = RequestSpinMagnitude
+    | ReceiveSpinMagnitude Int
+    | MSPassed Time
 
 
+view : Model -> Html Msg
+view model =
+    Svg.svg [ viewBox "-200 -200 400 400", width "400px", height "400px" ]
+        [ Svg.circle [ fill "blue", cx "0", cy "0", r "200", onClick RequestSpinMagnitude ] []
+        , Svg.polygon [ points "10,20 -10,20 0,-100 10,20"
+            , stroke "orange"
+            , strokeWidth "3"
+            , fill "none"
+            , strokeLinejoin "round"
+            , transform (String.concat [ "rotate(", (toString model.theta), " 0 0)" ])
+            ]
+            []
+        ]
 
+
+update : Msg -> Model -> ( Model, Cmd Msg )
+update msg model =
+    case msg of
+        RequestSpinMagnitude ->
+            ( model, Random.generate ReceiveSpinMagnitude (Random.int 100 500) )
+
+        ReceiveSpinMagnitude mag ->
+            ( { model | oomph = model.oomph + mag }, Cmd.none )
+
+        MSPassed _ ->
+            if model.oomph > 0 then
+                ( { model | theta = model.theta + 1, oomph = model.oomph - 1 }, Cmd.none )
+            else
+                ( model, Cmd.none )
+
+
+initialModel : Model
+initialModel =
+    { theta = 0
+    , oomph = 0
+    }
+
+
+subscriptions : Model -> Sub Msg
+subscriptions { theta, oomph } =
+    if oomph > 0 then
+        Time.every Time.millisecond MSPassed
+    else
+        Sub.none
+
+
+main =
+    Html.program
+        { init = ( initialModel, Cmd.none )
+        , view = view
+        , update = update
+        , subscriptions = subscriptions
+        }
